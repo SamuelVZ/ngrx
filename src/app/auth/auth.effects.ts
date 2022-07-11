23fb088df2334d4e8e7d4a90cc64ec87379eb585
Valencia <samuel.valencia.zamudio@hotmail.com>
2022-07-11 07:56:20 -0700

    add effects weak implementation

---
+++ src/app/auth/auth.effects.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { Actions } from "@ngrx/effects";
+
+@Injectable()
+export class AuthEffects {
+
+  constructor(private actions$: Actions){
+
+      actions$.subscribe(action => {
+        if(action.type == '[Login Page] user Login' ) {
+          localStorage.setItem('user', JSON.stringify(action["user"]));
+        }
+
+      });
+  }
+
+}
