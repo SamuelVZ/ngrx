4083965049f272925cfe8a170f6e65edf4a44086
Valencia <samuel.valencia.zamudio@hotmail.com>
2022-07-11 08:25:59 -0700

    implemented createEffects

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,17 +1,45 @@
 import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
+import { Router } from "@angular/router";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { tap } from "rxjs/operators";
+import { login, logout } from "./auth.actions";
 
 @Injectable()
 export class AuthEffects {
 
-  constructor(private actions$: Actions){
+  //createEffect manages error handling and there is no need to subscribe to it
+  login$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(login),
+      tap(action => {
+        localStorage.setItem('user', JSON.stringify(action.user));
+      })
+    ),
+    {dispatch: false} //this is for the action required after talking the backend, always needed
+  );
 
-      actions$.subscribe(action => {
-        if(action.type == '[Login Page] user Login' ) {
-          localStorage.setItem('user', JSON.stringify(action["user"]));
-        }
+  logout$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(logout),
+      tap(action => {
+        localStorage.removeItem('user');
 
-      });
+        this.router.navigateByUrl('/login');
+      })
+    ),
+    {dispatch: false}
+  );
+
+  constructor(private actions$: Actions,
+    private router: Router){
+    //old way implementation of effects
+
+      // actions$.subscribe(action => {
+      //   if(action.type == '[Login Page] user Login' ) {
+      //     localStorage.setItem('user', JSON.stringify(action["user"]));
+      //   }
+
+      // });
   }
 
 }
