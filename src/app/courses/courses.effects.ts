a890c85a3b471622269cc24a34768ed32e24ca7e
Valencia <samuel.valencia.zamudio@hotmail.com>
2022-07-12 09:56:55 -0700

    courses effects added

---
+++ src/app/courses/courses.effects.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { concatMap, map } from "rxjs/operators";
+import { CourseActions } from "./action-types";
+import { CoursesHttpService } from "./services/courses-http.service";
+
+@Injectable()
+export class CoursesEffects {
+
+  loadCourses$ = createEffect(
+    () => this.actions$.pipe(
+      ofType(CourseActions.loadAllCourses),
+      concatMap(action => this.coursesService.findAllCourses()), //send one request at the time to the backend (mergemap for multiple request)
+      map(courses => CourseActions.allCoursesLoaded({courses}))
+      )
+
+  );
+
+  constructor(
+    private actions$: Actions,
+    private coursesService: CoursesHttpService
+    ){
+
+  }
+
+}
