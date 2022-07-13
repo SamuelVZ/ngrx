1fedc4a6a846a0f069b3e0f0f17a8702de56452f
Valencia <2192592@cognizant.com>
2022-07-13 11:11:52 -0700

    have a flag to only load courses once, and not every time you visit the courses page

--- src/app/courses/courses.reducers.ts
+++ src/app/courses/courses.reducers.ts
@@ -1,21 +1,28 @@
 import { createEntityAdapter, EntityState } from "@ngrx/entity";
 import { createReducer, on } from "@ngrx/store";
 import { CourseActions } from "./action-types";
-import { Course } from "./model/course";
+import { compareCourses, Course } from "./model/course";
 
 
 export interface CoursesState extends EntityState<Course>{
+  allCoursesLoaded: boolean
 }
 
 
-export const adapter = createEntityAdapter<Course>();
+export const adapter = createEntityAdapter<Course>({
+  sortComparer: compareCourses,
+  // selectId: course => course.id //to select the id in case the name on the model is not id ex coursesId
+});
 
-export const initialCourseState = adapter.getInitialState();
+export const initialCourseState = adapter.getInitialState({
+  allCoursesLoaded: false
+});
 
 export const coursesReducer = createReducer(
   initialCourseState,
   on(CourseActions.allCoursesLoaded,
-    (state, action) => adapter.addMany(action.courses, state)
+    (state, action) => adapter.addMany(action.courses, {...state, allCoursesLoaded:true})
   )
 );
 
+export const { selectAll } = adapter.getSelectors();
